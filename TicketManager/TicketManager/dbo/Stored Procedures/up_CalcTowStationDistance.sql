218bd7bc3f3aeaa9ca57f0e2b4f3e001ed78bf35
Diluka <pmars42@gmail.com>
2014-03-03 14:12:09 +0800

    添加数据库架构项目

---
+++ TicketManager/TicketManager/dbo/Stored Procedures/up_CalcTowStationDistance.sql
@@ -0,0 +1,28 @@
+﻿CREATE PROCEDURE [dbo].[up_CalcTowStationDistance]
+	@AStationId int,
+	@BStationId int
+AS
+	DECLARE
+		@AX float,
+		@AY float,
+		@BX float,
+		@BY float
+
+	SELECT 
+		@AX=LocationX, @AY=LocationY
+	FROM
+		Station
+	WHERE
+		Station.StationId=@AStationId
+
+	SELECT 
+		@BX=LocationX, @BY=LocationY
+	FROM
+		Station
+	WHERE
+		Station.StationId=@BStationId
+
+	DECLARE @distance float
+	EXEC @distance = fn_CoordinateToDistance @AX,@AY,@BX,@BY
+	SELECT @distance
+RETURN 0
---
+++ TicketManager/TicketManager/dbo/Tables/Station.sql
@@ -0,0 +1,45 @@
+﻿CREATE TABLE [dbo].[Station] (
+    [StationId]   INT           IDENTITY (1, 1) NOT NULL,
+    [StationName] NVARCHAR (50) NULL,
+    [LocationX] FLOAT NULL, 
+    [LocationY] FLOAT NULL, 
+    CONSTRAINT [PK_Station] PRIMARY KEY CLUSTERED ([StationId] ASC)
+);
+
+
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'车站ID',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Station',
+    @level2type = N'COLUMN',
+    @level2name = N'StationId'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'车站名称',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Station',
+    @level2type = N'COLUMN',
+    @level2name = N'StationName'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'纬度',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Station',
+    @level2type = N'COLUMN',
+    @level2name = N'LocationX'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'经度',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Station',
+    @level2type = N'COLUMN',
+    @level2name = N'LocationY'
