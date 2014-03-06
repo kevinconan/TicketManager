1c4ca03fcce599f7d448165b1d8cfa50caa7f223
Diluka <pmars42@gmail.com>
2014-03-06 13:26:48 +0800

    bus ser更新，请修改action

--- src/kevin/tm/service/BusService.java
+++ src/kevin/tm/service/BusService.java
@@ -1,22 +1,26 @@
 package kevin.tm.service;
 
-
-
 import java.util.List;
 
 import kevin.tm.dao.model.BusBean;
-import kevin.tm.model.BusBeanExt;
+import kevin.tm.model.Bus;
 
 import org.springframework.stereotype.Service;
 
 @Service
 public interface BusService {
-	public int save(BusBean busBean);
-	public int deleteByVehicleNo(String vehicleNo);
-	public int deleteByVehicleNo(String[] vehivleNo);
-	public int update(BusBean busBean);
-	public List<BusBeanExt> findAll();
-	public BusBean findByVehicleNo(String vehicleNo);
+    public int save(BusBean busBean);
 
-	
+    public int deleteByVehicleNo(String vehicleNo);
+
+    public int deleteByVehicleNo(String[] vehivleNo);
+
+    public int update(BusBean busBean);
+
+    public List<Bus> findAll();
+
+    public Bus findByVehicleNo(String vehicleNo);
+
+    public int count();
+
 }
