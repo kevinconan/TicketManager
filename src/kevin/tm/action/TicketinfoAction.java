198136a1c6ff93495938f48fa0d9911046ff85d9
Diluka <pmars42@gmail.com>
2014-03-15 23:02:55 +0800

    需要测试

--- src/kevin/tm/action/TicketinfoAction.java
+++ src/kevin/tm/action/TicketinfoAction.java
@@ -1,27 +1,25 @@
 /**
- * 
+ *
  */
 package kevin.tm.action;
 
 import java.util.HashMap;
-
 import kevin.tm.dao.model.Ticketinfo;
 import kevin.tm.service.TicketInfoService;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 
 /**
  * @author Diluka
- * 
+ *
  */
 @Scope("request")
 @Controller("ticketinfoAction")
 public class TicketinfoAction extends BaseAction<Ticketinfo> {
 
     /**
-     * 
+     *
      */
     private static final long serialVersionUID = -6572387338226312913L;
 
@@ -29,11 +27,21 @@ public class TicketinfoAction extends BaseAction<Ticketinfo> {
     private TicketInfoService service;
 
     public String list() {
-	this.list = this.service.findByPage(this.start, this.limit);
+        this.list = this.service.findByPage(this.start, this.limit);
 
-	this.map = new HashMap<String, Object>();
-	this.map.put(TOTAL_COUNT, this.service.totalCount());
-	this.map.put(DATA, this.list);
-	return MAP;
+        this.map = new HashMap<>();
+        this.map.put(TOTAL_COUNT, this.service.totalCount());
+        this.map.put(DATA, this.list);
+        return MAP;
+    }
+
+    public String listByScheduleid() {
+        Integer id = Integer.valueOf(GSON.fromJson(message, String.class));
+        list = service.findByScheduleid(id, start, limit);
+
+        this.map = new HashMap<>();
+        this.map.put(TOTAL_COUNT, this.service.countByScheduleid(id));
+        this.map.put(DATA, this.list);
+        return MAP;
     }
 }
