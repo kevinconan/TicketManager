df66f9c956984eef2709cd606ddd42b71144b1a3
Diluka <pmars42@gmail.com>
2014-03-18 14:55:18 +0800

    像这样判断是不是空格好不好？
    全部都被格式化了一下= =

--- src/kevin/tm/util/ValidationUtil.java
+++ src/kevin/tm/util/ValidationUtil.java
@@ -1,122 +1,124 @@
 package kevin.tm.util;
 
+import com.google.gson.JsonElement;
 import java.util.List;
 import java.util.Map;
 
-import com.google.gson.JsonElement;
-
 /**
  * 各种数据类型验证工具类
- * 
+ *
  * @author wuge
- * 
+ *
  */
 public class ValidationUtil {
-	/**
-	 * 对象的非空校验
-	 * 
-	 * @param value
-	 * @return
-	 */
-	public static boolean isNullOrEmpty(Object value) {
-		return value == null;
-	}
 
-	/**
-	 * 字符串对象的非空校验
-	 * 
-	 * @param value
-	 * @return
-	 */
-	public static boolean isNullOrEmpty(String value) {
-		return value == null || value.isEmpty();
-	}
-	/**
-	 * 字符串对象的非空校验
-	 * 
-	 * @param value
-	 * @return
-	 */
-	public static boolean isNullOrEmpty1(String value) {
-		return value == null || value.isEmpty()||value.equals("")||value.equals(" ");
-	}
-	
-	/**
-	 * 字符串对象的非空校验
-	 * 
-	 * @param value
-	 * @return
-	 */
-	public static boolean isNullOrEmptyStr(String value) {
-		return value == null || value.isEmpty()||value.equals("")||value.equals("[]");
-	}
-	
-	/**
-	 * JsonElement对象的非空校验
-	 * 
-	 * @param value
-	 * @return
-	 */
-	public static boolean isNullOrEmptyJson(JsonElement value) {
-		return value == null || value.getAsString().isEmpty()||value.getAsString().equals("")||value.getAsString().equals(" ");
-	}
+    /**
+     * 对象的非空校验
+     *
+     * @param value
+     * @return
+     */
+    public static boolean isNullOrEmpty(Object value) {
+        return value == null;
+    }
 
-	/**
-	 * 整形对象的非空校验
-	 * 
-	 * @param value
-	 * @return
-	 */
-	public static boolean isNullOrZero(Integer value) {
-		return value == null || value == 0;
-	}
+    /**
+     * 字符串对象的非空校验
+     *
+     * @param value
+     * @return
+     */
+    public static boolean isNullOrEmpty(String value) {
+        return value == null || value.isEmpty();
+    }
 
-	/**
-	 * 长整形对象的非空校验
-	 * 
-	 * @param value
-	 * @return
-	 */
-	public static boolean isNullOrZero(Long value) {
-		return value == null || value == 0;
-	}
+    /**
+     * 字符串对象的非空校验
+     *
+     * @param value
+     * @return
+     */
+    public static boolean isNullOrEmpty1(String value) {
+        return value == null || value.isEmpty() || value.equals("") || value.equals(" ");
+    }
 
-	/**
-	 * 布尔对象的非空校验
-	 * 
-	 * @param value
-	 * @return
-	 */
-	public static boolean isNullOrFalse(Boolean value) {
-		return value == null || !value;
-	}
+    /**
+     * 字符串对象的非空校验
+     *
+     * @param value
+     * @return
+     */
+    public static boolean isNullOrEmptyStr(String value) {
+        return value == null || value.isEmpty() || value.equals("") || value.equals("[]");
+    }
 
-	/**
-	 * 数组的非空校验
-	 * 
-	 * @param list
-	 * @return
-	 */
-	public static <T extends Object> boolean isNullOrEmpty(T[] array) {
-		return array == null || array.length == 0;
-	}
+    /**
+     * JsonElement对象的非空校验
+     *
+     * @param value
+     * @return
+     */
+    public static boolean isNullOrEmptyJson(JsonElement value) {
+        return value == null || value.getAsString().isEmpty() || value.getAsString().matches("\"\\s*\"");//像这样判断是不是空格好不好？
+    }
 
-	/**
-	 * 对象集合的非空校验
-	 * 
-	 * @param list
-	 * @return
-	 */
-	public static boolean isNullOrEmpty(List<? extends Object> list) {
-		return list == null || list.isEmpty();
-	}
-	
-	/**
-	 * 对象键值对集合的非空校验
-	 * @param map
-	 * @return
-	 */
-	public static boolean isNullOrEmpty(Map<? extends Object, ? extends Object> map){
-		return map == null || map.size() == 0;
-	}
+    /**
+     * 整形对象的非空校验
+     *
+     * @param value
+     * @return
+     */
+    public static boolean isNullOrZero(Integer value) {
+        return value == null || value == 0;
+    }
+
+    /**
+     * 长整形对象的非空校验
+     *
+     * @param value
+     * @return
+     */
+    public static boolean isNullOrZero(Long value) {
+        return value == null || value == 0;
+    }
+
+    /**
+     * 布尔对象的非空校验
+     *
+     * @param value
+     * @return
+     */
+    public static boolean isNullOrFalse(Boolean value) {
+        return value == null || !value;
+    }
+
+    /**
+     * 数组的非空校验
+     *
+     * @param list
+     * @return
+     */
+    public static <T extends Object> boolean isNullOrEmpty(T[] array) {
+        return array == null || array.length == 0;
+    }
+
+    /**
+     * 对象集合的非空校验
+     *
+     * @param list
+     * @return
+     */
+    public static boolean isNullOrEmpty(List<? extends Object> list) {
+        return list == null || list.isEmpty();
+    }
+
+    /**
+     * 对象键值对集合的非空校验
+     *
+     * @param map
+     * @return
+     */
+    public static boolean isNullOrEmpty(Map<? extends Object, ? extends Object> map) {
+        return map == null || map.size() == 0;
+    }
 }
