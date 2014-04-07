fc053219e130b0ab291f6ec477bb1354377e1938
Diluka <pmars42@gmail.com>
2014-04-07 15:42:36 +0800

    不科学
    这东西一定不能存在于这个星球上

--- src/kevin/tm/util/ReflectUtil.java
+++ src/kevin/tm/util/ReflectUtil.java
@@ -4,16 +4,14 @@ import java.lang.reflect.Field;
 
 public class ReflectUtil {
 
-	public static String[] getMembers(Object o){
-		
-		Field[] fields = o.getClass().getDeclaredFields();
-		String[] members = new String[fields.length];
-		for(int i=0;i < fields.length;i++){
-			members[i] = fields[i].getName();
-		}
-		
-		
-		
-		return members;
-	}
+    public static String[] getMembers(Object o) {
+
+        Field[] fields = o.getClass().getDeclaredFields();
+        String[] members = new String[fields.length];
+        for (int i = 0; i < fields.length; i++) {
+            members[i] = fields[i].getName();
+        }
+
+        return members;
+    }
 }
